907d599edbea42b330e853537e1c39fe1055f2dd
Tomas Maldonado <tomasingame@gmail.com>
2022-04-04 03:23:04 -0300

    solved odontrologo problem

--- src/main/java/com/ctd/finalbackend1/model/entity/Odontologo.java
+++ src/main/java/com/ctd/finalbackend1/model/entity/Odontologo.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "odontrologo")
+@Table(name = "odontologo")
 @Setter @Getter
 public class Odontologo {
     @Id
